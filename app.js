e0e6c155e9db9d1245c98d61d63ce29dbd7d5038
Spacecowboi <cunningham2192@gmail.com>
2023-07-11 15:40:09 -0700

    added user name functionality, removed button from css as I couldn't figure out why it's not loading, even though the linter said everything looked good. working out why no response is giving ys response

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 'use strict';
 
+function greetUser(){
+  let name = prompt('What is your name?');
+  alert('Welcome to the site, ' + name + '!');
+}
+
+greetUser();
+
 function fixInput(input) {
 
-        return input.toLowerCase();
+  return input.toLowerCase();
 }
 
 // Validating yes or no
 function validaton(response) {
 
-        response = fixInput(response);
-        return response === 'yes' || response === 'no; 
+  response = fixInput(response);
+  return response === 'yes' || response === 'no';
 }
+
+// prompt then validate the user responses
+function askQuestion(question){
+
+  let response = prompt(question);
+
+  if (validaton(response)){
+    //console.log(response);
+    alert('Awesome!');
+  } else {
+    //console.log(response);
+    alert('Oof size large m8');
+  }
+}
+
+askQuestion('First question: Do you like anime?' );
+askQuestion('Second question: Have you ever traveled?');
+askQuestion('Third question: Do you like video games?');
+askQuestion('Fourth question: Have you ever run a marathon');
+askQuestion('Fifth question: Do you enjoy cannabis?');
