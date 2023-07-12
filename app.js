ee5520a7dd90916bc020a1683e3f36cc1918918e
Spacecowboi <cunningham2192@gmail.com>
2023-07-12 14:11:06 -0700

    added question 6 requirements, working out why user is still prompted after more than 4 attempts

--- app.js
+++ app.js
@@ -40,3 +40,37 @@ askQuestion('Second question: Have you ever traveled?', 'yes');
 askQuestion('Third question: Do you like video games?', 'yes');
 askQuestion('Fourth question: Have you ever run a marathon', 'yes');
 askQuestion('Fifth question: Do you enjoy cannabis?', 'yes');
+
+//Adding guessing game question for question 6
+
+function guessaNumber(){
+
+  let attempts = 4;
+  let rightAnswer = 42;
+
+  while (attempts > 0) {
+    let guess = +prompt('Time for some numbers! Guess a number (between 1 and 100):');
+
+    if (Number.isInteger(guess) && guess >=1 && guess <= 100){
+      if (guess > rightAnswer){
+        alert('A little too high!');
+
+      } else if (guess < rightAnswer){
+
+        alert ('Too low!');
+
+      } else {
+
+        alert('You got it!');
+
+        return;
+      }
+    } else {
+
+      alert('Please enter a real number.');
+    }
+  }
+  alert('You gave it all you had. The correct answer is ' + rightAnswer + '.');
+}
+
+guessaNumber();
